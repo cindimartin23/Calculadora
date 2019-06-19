727b30a6a9ae162d276f34cb75b7290dd5b81355
Cindi <cindimartin23@hotmail.com>
2019-06-19 16:51:03 -0300

    Update app.js
    Agregado código para leer cuando se presiona un número y se muestre en la pantalla.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var Calculadora = {
+    visor: document.getElementById("display"),
+    numeroEnVisor: "0",
+    
     init: function(){
         this.formatoTeclas();
+        this.asignarEventos();
     },
 
     // Formato de botones cuando se presiona y suelta una tecla
@@ -17,7 +21,8 @@ var Calculadora = {
 
 	maximizaTeclaEvento: function(event){
 		Calculadora.maximizaTecla(event.target);
-	},
+    },
+    /* Para achicar la tecla */ 
     minimizaTecla: function(boton){
         var idBoton = boton.id;
         if( idBoton == "1" || 
@@ -36,6 +41,7 @@ var Calculadora = {
             boton.style.height = "62px";
         }
     },
+    /* Para agrandar la tecla */
     maximizaTecla: function(boton) {
         var idBoton = boton.id;
         if( idBoton == "1" || 
@@ -53,7 +59,56 @@ var Calculadora = {
 		boton.style.width = "22%";
 		boton.style.height = "62.91px";
 		}
-    }
+    },
+    asignarEventos: function(){
+		document.getElementById("0").addEventListener("click", function() {
+            Calculadora.ingresarNumero("0");
+        });
+		document.getElementById("1").addEventListener("click", function() {
+            Calculadora.ingresarNumero("1");
+        });
+		document.getElementById("2").addEventListener("click", function() {
+            Calculadora.ingresarNumero("2");
+        });
+		document.getElementById("3").addEventListener("click", function() {
+            Calculadora.ingresarNumero("3");
+        });
+		document.getElementById("4").addEventListener("click", function() {
+            Calculadora.ingresarNumero("4");
+        });
+		document.getElementById("5").addEventListener("click", function() {
+            Calculadora.ingresarNumero("5");
+        });
+		document.getElementById("6").addEventListener("click", function() {
+            Calculadora.ingresarNumero("6");
+        });
+		document.getElementById("7").addEventListener("click", function() {
+            Calculadora.ingresarNumero("7");
+        });
+		document.getElementById("8").addEventListener("click", function() {
+            Calculadora.ingresarNumero("8");
+        });
+		document.getElementById("9").addEventListener("click", function() {
+            Calculadora.ingresarNumero("9");
+        });
+    },
+    /* Funcion para leer el numero presionado */ 
+    ingresarNumero: function(numero){
+		if (this.numeroEnVisor.length < 8) {
+		
+			if (this.numeroEnVisor == "0") {
+				this.numeroEnVisor = "";
+				this.numeroEnVisor = this.numeroEnVisor + numero;
+			} else {
+				this.numeroEnVisor = this.numeroEnVisor + numero;
+			}
+		this.actualizarVisor();
+		}
+    },
+    /* Funcion para refrescar los datos en la pantalla */ 
+    actualizarVisor: function(){
+		this.visor.innerHTML = this.numeroEnVisor;
+	}
 
 };
 
